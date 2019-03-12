111b639c010d50ace9ca7625e5b468859fe704f2
Ebrima saho <saho.ebrima@ymail.com>
2019-03-11 20:12:50 -0700

    Logic works fine. Need to add styles to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,88 @@
+var userGuess= "";
+var guessesSoFar= [];
+var ComGuess="";
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
 
+//variables to hold dispplays on screem
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var guessesLeftText = document.getElementById("guessesLeft");
+var randomLetter = randomLetterGen();
+var guessesSoFarText = document.getElementById("guessesSoFar");
 
 function randomLetterGen(){
     var alphabetalLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return alphabetalLetters.charAt(Math.floor(Math.random()*alphabetalLetters.length));
+    ComGuess = alphabetalLetters.charAt(Math.floor(Math.random()*alphabetalLetters.length));
 }
 
 function guessingGame(userGuess){
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 10;
-    //variables to hold dispplays on screem
-    var winsText = document.getElementById("wins");
-    var lossesText = document.getElementById("losses");
-    var guessesLeftText = document.getElementById("guessesLeft");
-    var randomLetter = randomLetterGen();
+   
+   // var  guessesSoFarText   = document.getElementById("userGuess").value;
     console.log(randomLetter);
     
+    winsText.textContent =  wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;
+    guessesSoFarText.textContent =  guessesSoFar.join(",");
+    console.log(userGuess);
+   
+}
+
+document.onkeyup = function(event) {
+    // Determines which key was pressed.
     
+     userGuess = event.key;
+    //console.log(userGuess);
+    guessingGame(userGuess);
+    guessesLeft--;
+    guessesLeftText.textContent = guessesLeft;
+    guessesSoFar.push(userGuess);
+    console.log(guessesSoFar);
+
     if (userGuess===randomLetter){
         wins++;
+        guessesLeft=10;
+        winsText.textContent =  wins;
+        guessesLeftText.textContent = guessesLeft;
+
     
     }
-    else{
+    //  else{
+    //     guessesLeft--;
+    //     guessesLeftText.textContent = guessesLeft;
+        
+    // }
+    
+
+    if (guessesLeft===0){
         losses++;
-        guessesLeft--;
+        guessesLeft=10;
+        lossesText.textContent = losses;
+        
+        guessesSoFar.length=0
+        guessesLeftText.textContent = guessesLeft;
     }
+    
 
     console.log(wins);
     console.log(losses);
     console.log(guessesLeft);
     
     
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+   /* winsText.textContent =  wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;*/
+    guessesSoFarText.textContent =  guessesSoFar.join(",");
+
+    
     console.log(userGuess);
 
     
 }
-
-document.onkeyup = function(event) {
-    // Determines which key was pressed.
     
-    var userGuess = event.key;
-    //console.log(userGuess);
-    guessingGame(userGuess);
-    
-}
+    randomLetterGen();
 
     
 

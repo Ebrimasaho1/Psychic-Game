12e75fc59d3f9c15b0555ace8d098f46e8e924be
Ebrima saho <saho.ebrima@ymail.com>
2019-03-12 18:35:51 -0700

    Game working, but still need to make the UI better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,42 @@
-var userGuess= "";
-var guessesSoFar= [];
-var ComGuess="";
+
+//declaring global variables 
+var userGuess = "";
+var guessesSoFar = [];
+var ComGuess = "";
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 
-//variables to hold dispplays on screem
+//variables to hold dispplays on screem. Basically variables to accesss the html tags
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesLeftText = document.getElementById("guessesLeft");
 var randomLetter = randomLetterGen();
 var guessesSoFarText = document.getElementById("guessesSoFar");
 
-function randomLetterGen(){
+//function that generates the random letters used to compare with user input
+function randomLetterGen() {
     var alphabetalLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    ComGuess = alphabetalLetters.charAt(Math.floor(Math.random()*alphabetalLetters.length));
+    ComGuess = alphabetalLetters.charAt(Math.floor(Math.random() * alphabetalLetters.length));
 }
 
-function guessingGame(userGuess){
-   
-   // var  guessesSoFarText   = document.getElementById("userGuess").value;
+function guessingGame(userGuess) {
+
+    // var  guessesSoFarText   = document.getElementById("userGuess").value;
     console.log(randomLetter);
-    
-    winsText.textContent =  wins;
+
+    winsText.textContent = wins;
     lossesText.textContent = losses;
     guessesLeftText.textContent = guessesLeft;
-    guessesSoFarText.textContent =  guessesSoFar.join(",");
+    guessesSoFarText.textContent = guessesSoFar.join(",");
     console.log(userGuess);
-   
+
 }
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
+   
     // Determines which key was pressed.
-    
-     userGuess = event.key;
+    userGuess = event.key;
     //console.log(userGuess);
     guessingGame(userGuess);
     guessesLeft--;
@@ -41,51 +44,53 @@ document.onkeyup = function(event) {
     guessesSoFar.push(userGuess);
     console.log(guessesSoFar);
 
-    if (userGuess===randomLetter){
+    if (userGuess === randomLetter) {
         wins++;
-        guessesLeft=10;
-        winsText.textContent =  wins;
+        guessesLeft = 10;
+        winsText.textContent = wins;
         guessesLeftText.textContent = guessesLeft;
 
-    
+
     }
     //  else{
     //     guessesLeft--;
     //     guessesLeftText.textContent = guessesLeft;
-        
-    // }
-    
 
-    if (guessesLeft===0){
+    // }
+
+    // conditional statememt that tells the guessesLeft to reset to it's initial setting when the guessesLeft reaches 0
+    if (guessesLeft === 0) {
         losses++;
-        guessesLeft=10;
+        guessesLeft = 10;
         lossesText.textContent = losses;
-        
-        guessesSoFar.length=0
+
+        guessesSoFar.length = 0
         guessesLeftText.textContent = guessesLeft;
+        
     }
-    
+
 
     console.log(wins);
     console.log(losses);
     console.log(guessesLeft);
-    
-    
-   /* winsText.textContent =  wins;
-    lossesText.textContent = losses;
-    guessesLeftText.textContent = guessesLeft;*/
-    guessesSoFarText.textContent =  guessesSoFar.join(",");
 
-    
+    //this .join() method is used to insert the comma in the array. 
+    guessesSoFarText.textContent = guessesSoFar.join(",");
     console.log(userGuess);
-
-    
 }
-    
+    //calling the function that generates the random letters
     randomLetterGen();
 
-    
+    //function for resetting game
+function gameReset() {
+    wins = 0;
+    losses = 0;
+    guessesLeft = 10;
+    guessesSoFar.length=0
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;
+    guessesSoFarText.textContent = guessesSoFar;
 
-
-
+};
 
